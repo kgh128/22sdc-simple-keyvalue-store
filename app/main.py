86edd31b6711962189a87bfff4ef8b447195ee1d
kgh128 <hayoung0627@gmail.com>
2022-08-26 00:04:38 +0900

    FEAT : 예외 상황 plain text로 response

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from app.routers import item
 
 app = FastAPI()
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=422)
+
 
 app.include_router(item.router)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return PlainTextResponse(str("Simple Key-Value Store"))
