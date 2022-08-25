5bc280a63ca025008d3c7438231ab956fa0b1aec
kgh128 <hayoung0627@gmail.com>
2022-08-26 00:49:54 +0900

    FEAT : Put, Delete Item 기능 구현

--- app/services/item.py
+++ app/services/item.py
@@ -13,5 +13,24 @@ class ItemService:
     def get_item(key: int):
         if key not in store:
             raise HTTPException(status_code=404, detail="Item not found")
-        item = {"key": key, "value": store[key]}
+
+        item = {
+            "description": "Get item",
+            "key": key,
+            "value": store[key]}
         return Item(**item)
+
+    def put_item(item: Item):
+        store[item.key] = item.value
+
+        if item.key not in store:
+            item.description = "Put item"
+        item.description = "Update item"
+
+        return item
+
+    def delete_item(key: int):
+        item = ItemService.get_item(key)
+        item.description = "Delete item"
+        del store[key]
+        return item
