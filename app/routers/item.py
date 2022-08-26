c27a5d3dacda52a6cbd663563d3795520f97c106
kgh128 <hayoung0627@gmail.com>
2022-08-26 17:33:44 +0900

    REFACTOR : Item Service
    ItemCRUD 클래스를 추가하여 Item Service를 구조화시켜 깔끔하게 만들었다.

--- app/routers/item.py
+++ app/routers/item.py
@@ -12,14 +12,19 @@ router = APIRouter(
 
 @router.get("/{key}", response_model=Item)
 async def get_item(key: int):
-    return ItemService.get_item(key)
+    return ItemService().get_item(key)
 
 
 @router.post("/", response_model=Item)
 async def put_item(item: Item):
-    return ItemService.put_item(item)
+    return ItemService().put_item(item)
+
+
+@router.put("/", response_model=Item)
+async def update_item(item: Item):
+    return ItemService().update_item(item)
 
 
 @router.delete("/{key}", response_model=Item)
 async def delete_item(key: int):
-    return ItemService.delete_item(key)
+    return ItemService().delete_item(key)
--- app/services/item.py
+++ app/services/item.py
@@ -1,37 +1,72 @@
 from fastapi import HTTPException
 
 from app.schemas.item import Item
-# from app.utils.appExceptions import AppException
-# from app.utils.serviceResult import ServiceResult
-
-# 데이터 저장 dictionary
-store = {1: "hello"}
 
 
 class ItemService:
     # noinspection PyMethodMayBeStatic
-    def get_item(key: int):
-        if key not in store:
-            raise HTTPException(status_code=404, detail="Item not found")
-
-        item = {
-            "description": "Get item",
-            "key": key,
-            "value": store[key]}
-        return Item(**item)
-
-    def put_item(item: Item):
-        if item.key not in store:
-            item.description = "Put item"
-        else:
-            item.description = "Update item"
-
-        store[item.key] = item.value
-
+    def get_item(self, key: int) -> Item:
+        item = ItemCRUD().get_item(key)
+        if not item:
+            raise HTTPException(status_code=404, detail="해당 key가 존재하지 않아 value를 찾을 수 없습니다.")
         return item
 
-    def delete_item(key: int):
-        item = ItemService.get_item(key)
-        item.description = "Delete item"
-        del store[key]
+    # noinspection PyMethodMayBeStatic
+    def put_item(self, item: Item) -> Item:
+        item = ItemCRUD().put_item(item)
+        if not item:
+            raise HTTPException(status_code=422, detail="해당 key가 이미 존재하여 value를 새로 입력할 수 없습니다.")
+        return item
+
+    # noinspection PyMethodMayBeStatic
+    def update_item(self, item: Item) -> Item:
+        item = ItemCRUD().update_item(item)
+        if not item:
+            raise HTTPException(status_code=404, detail="해당 key가 존재하지 않아 value를 업데이트 할 수 없습니다.")
+        return item
+
+    # noinspection PyMethodMayBeStatic
+    def delete_item(self, key: int) -> Item:
+        item = ItemCRUD().delete_item(key)
+        if not item:
+            raise HTTPException(status_code=404, detail="해당 key가 존재하지 않아 value를 삭제할 수 없습니다.")
+        return item
+
+
+class ItemCRUD:
+    # 데이터 저장 dictionary
+    store: dict[int, str] = {}
+
+    # noinspection PyMethodMayBeStatic
+    def create_item(self, description: str, key: int, value: str):
+        item = {"description": description,
+                "key": key,
+                "value": value}
+        return Item(**item)
+
+    def get_item(self, key: int):
+        if key not in self.store:
+            return None
+        item = self.create_item("Get item", key, self.store[key])
+        return item
+
+    def put_item(self, item: Item):
+        if item.key in self.store:
+            return None
+        item = self.create_item("Put item", item.key, item.value)
+        self.store[item.key] = item.value
+        return item
+
+    def update_item(self, item: Item):
+        if item.key not in self.store:
+            return None
+        item = self.create_item("Update item", item.key, item.value)
+        self.store[item.key] = item.value
+        return item
+
+    def delete_item(self, key: int):
+        if key not in self.store:
+            return None
+        item = self.create_item("Delete item", key, self.store[key])
+        del self.store[key]
         return item
