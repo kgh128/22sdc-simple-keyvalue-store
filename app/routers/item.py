9103fc709469f54873c06d2e048545408de10888
kgh128 <hayoung0627@gmail.com>
2022-09-03 09:07:11 +0900

    FEAT: ResponseDTO 생성 및 적용
    - 성공 했을 시 response 형식을 다 맞추기 위해 ResPonseDTO와 GetResponseDTO 생성
    - ItemService와 라우터에 적용

--- app/routers/item.py
+++ app/routers/item.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.schemas.item import Item
-from app.schemas.serviceResult import ServiceResult
+from app.schemas.responseDTO import ResponseDTO, GetResponseDTO
 from app.services.item import ItemService
 
 router = APIRouter(
@@ -11,26 +11,26 @@ router = APIRouter(
 )
 
 
-@router.get("/{key}", response_model=ServiceResult)
+@router.get("/{key}", response_model=GetResponseDTO)
 async def get_item(key: int):
     return ItemService().get_item(key)
 
 
-@router.get("/", response_model=ServiceResult)
+@router.get("/", response_model=GetResponseDTO)
 async def get_all_items():
     return ItemService().get_all_items()
 
 
-@router.post("/")
-async def put_item(item: Item):
+@router.post("/", response_model=ResponseDTO)
+async def set_item(item: Item):
     return ItemService().set_item(item)
 
 
-@router.delete("/{key}")
+@router.delete("/{key}", response_model=ResponseDTO)
 async def delete_item(key: int):
     return ItemService().delete_item(key)
 
 
-@router.delete("/")
+@router.delete("/", response_model=ResponseDTO)
 async def delete_all_items():
     return ItemService().delete_all_items()
